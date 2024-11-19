979c3607af59d95b91191fd7ddd47132b1c23555
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-19 16:03:26 +0900

    Feat: 사이드바 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "components/Header";
+import SideBarLeft from "components/SideBarLeft";
 import React from "react";
 
 export default function App() {
   return (
     <div>
       <Header></Header>
+      <SideBarLeft></SideBarLeft>
     </div>
   );
 }
