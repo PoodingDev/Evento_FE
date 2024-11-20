940cd8ed7edc4d9adcf207c6278e3b11f6a9b59a
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-20 23:39:53 +0900

    Chore: #10 Tailwind 색상 팔레드 정의
    - `eventoPurpleLight` (#EADDFF)
    - `eventoPurpleBase` (#6750A4)
    - `eventoPurpleDark` (#4F378B)
    - `eventoPurple` (#8867DF) -> 기본 색상
    - `eventoWhite` (#FCFCFA)
    - `lightRed` (#FFCFCB)
    - `darkRed` (#E13228)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       screens: {
         xs: "380px",
       },
+      colors: {
+        // 주요 색상
+        eventoPurple: "#8867DF", //기본 이벤토 색상
+        eventoWhite: "#FCFCFA",
+
+        // 보라색 색상들
+        eventoPurpleLight: "#EADDFF",
+        eventoPurpleBase: "#6750A4",
+        eventoPurpleDark: "#4F378B",
+
+        // 경고 색상
+        lightRed: "#FFCFCB",
+        darkRed: "#E13228",
+      },
     },
   },
+  plugins: [],
 };
