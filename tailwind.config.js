9b192d0f60a16e552103e9051dc2138fe1f3275c
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-21 00:56:26 +0900

    Feat: #8 로그인 페이지 UI 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Pretendard", "ui-sans-serif", "system-ui"],
+      },
       screens: {
         xs: "380px",
       },
