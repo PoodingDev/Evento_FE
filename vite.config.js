16843848a6a76cbeb787d2188c0a262ab8ba7521
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-21 19:16:52 +0900

    Feat: #9 구글 인가 코드 POST

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
