593c9022be63001ab6c679986cbc629f94cb4536
sujinnn <nmnm0919@naver.com>
2024-11-22 12:07:05 +0900

    Feat: #7 rem으로 단위 수정

--- src/components/EventInfoModal.jsx
+++ src/components/EventInfoModal.jsx
@@ -11,36 +11,40 @@ import {
 
 export default function EventInfo() {
   return (
-    <div className="ml-[18rem] flex h-screen items-center justify-center">
-      <div className="h-[450px] w-[750px] rounded-[20px] bg-eventoWhite p-[40px] shadow-[0px_0px_10px_gray]">
+    <div className="ml-[18rem] flex h-screen items-center justify-center pt-[5rem]">
+      <div className="h-[28rem] w-[48rem] rounded-[2.5rem] bg-eventoWhite p-[2.8rem] shadow-[0_0_2.5rem_gray]">
         <div className="flex flex-wrap justify-between">
           {/* 제목 */}
-          <div className="text-[64px] font-bold">저녁 약속</div>
-          <div className="p-[25px_380px_0px_0px]">
+          <div className="text-[2.8rem] font-bold">저녁 약속</div>
+          <div className="p-[0.3rem_25rem_2rem_0]">
             <FaLock size={25} />
           </div>
           <FaXmark size={25} />
         </div>
-        <div className="bg-eventoYellow mb-[50px] w-[160px] rounded-[50px] text-center text-[20px] font-bold">
+        <div className="bg-eventoYellow mb-[3.3rem] h-[1.5rem] w-[10rem] rounded-[3rem] text-center text-[1.2rem] font-bold leading-[1.7rem]">
           Pooding팀
         </div>
 
         {/* 시간 */}
-        <div className="text-[16px] font-bold text-eventoPurple">시간</div>
-        <div className="mb-[20px] w-[700px] text-[32px] font-medium">
+        <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
+          시간
+        </div>
+        <div className="mb-[2rem] w-[40rem] text-[2rem] font-medium">
           2024-9-10 - 2024-9-10
         </div>
 
         {/* 일정 상세 */}
-        <div className="text-[16px] font-bold text-eventoPurple">일정 상세</div>
-        <div className="w-[700px] pb-[100px] text-[24px] font-medium">
+        <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
+          일정 상세
+        </div>
+        <div className="w-[40rem] pb-[3.5rem] text-[1.5rem] font-medium">
           고기 먹자!
         </div>
 
         {/* 기타 아이콘 */}
         <div className="flex flex-wrap justify-between">
           <FaCommentAlt size={25} />
-          <div className="pr-[500px]">
+          <div className="pr-[32rem]">
             <FaBookmark size={25} />
           </div>
           <FaRegTrashAlt size={25} />
