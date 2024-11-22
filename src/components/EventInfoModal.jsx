caf6ee12612caa0e623456c32aecd55bf7644a39
sujinnn <nmnm0919@naver.com>
2024-11-22 18:06:51 +0900

    Feat: #6 이벤트 수정 모달 UI 구현

--- src/components/EventInfoModal.jsx
+++ src/components/EventInfoModal.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaXmark } from "react-icons/fa6";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import {
   FaLock,
@@ -7,49 +9,122 @@ import {
   FaRegTrashAlt,
   FaPen,
   FaCommentAlt,
+  FaCaretDown,
+  FaToggleOn,
 } from "react-icons/fa";
 
 export default function EventInfo() {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setendDate] = useState(new Date());
+
   return (
     <div className="ml-[18rem] flex h-screen items-center justify-center pt-[5rem]">
       <div className="h-[28rem] w-[48rem] rounded-[2.5rem] bg-eventoWhite p-[2.8rem] shadow-[0_0_2.5rem_gray]">
         <div className="flex flex-wrap justify-between">
-          {/* 제목 */}
-          <div className="text-[2.8rem] font-bold">저녁 약속</div>
-          <div className="p-[0.3rem_25rem_2rem_0]">
-            <FaLock size={25} />
-          </div>
+          {/* 이벤트 제목 */}
+          {isEdit ? (
+            <div className="flex">
+              <div className="h-[3.8rem] pb-[1rem] text-[2.8rem] font-bold">
+                저녁 약속
+              </div>
+              <div className="p-[0.3rem_25rem_0_1rem]">
+                <FaLock size={25} />
+              </div>
+            </div>
+          ) : (
+            <input
+              type="text"
+              placeholder="일정을 입력하세요"
+              className="h-[3.8rem] w-[30rem] pb-[1rem] text-[2.8rem] font-bold"
+            />
+          )}
           <FaXmark size={25} />
         </div>
-        <div className="bg-eventoYellow mb-[3.3rem] h-[1.5rem] w-[10rem] rounded-[3rem] text-center text-[1.2rem] font-bold leading-[1.7rem]">
-          Pooding팀
-        </div>
+
+        {/* 캘린더 제목 */}
+        {isEdit ? (
+          <div className="bg-eventoYellow mb-[3.3rem] h-[1.5rem] w-[10rem] rounded-[3rem] text-center text-[1.2rem] font-bold leading-[1.7rem]">
+            Pooding팀
+          </div>
+        ) : (
+          <div className="bg-eventoYellow mb-[3.3rem] flex h-[1.5rem] w-[10rem] justify-center rounded-[3rem] text-center text-[1.2rem] font-bold leading-[1.7rem]">
+            <FaCaretDown size={25} />
+            <p>Pooding팀</p>
+          </div>
+        )}
 
         {/* 시간 */}
         <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
           시간
         </div>
-        <div className="mb-[2rem] w-[40rem] text-[2rem] font-medium">
-          2024-9-10 - 2024-9-10
-        </div>
+        {isEdit ? (
+          <div className="mb-[1.8rem] h-[2rem] w-[24rem] text-[2rem] font-medium">
+            2024-09-10 - 2024-09-10
+          </div>
+        ) : (
+          <div className="mb-[1.8rem] flex h-[2rem] text-[2rem] font-medium">
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              dateFormat="yyyy-MM-dd"
+              className="m-[0] w-[11rem]"
+            />
+            <p className="mr-[1rem] w-[1rem]">-</p>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setendDate(date)}
+              dateFormat="yyyy-MM-dd"
+              className="w-[11rem]"
+            />
+          </div>
+        )}
 
         {/* 일정 상세 */}
         <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
           일정 상세
         </div>
-        <div className="w-[40rem] pb-[3.5rem] text-[1.5rem] font-medium">
-          고기 먹자!
-        </div>
+        {isEdit ? (
+          <div className="mb-[3.5rem] h-[1.8rem] w-[40rem] text-[1.5rem] font-medium">
+            고기 먹자!
+          </div>
+        ) : (
+          <input
+            type="text"
+            placeholder="무슨 일정인가요?"
+            className="mb-[3.5rem] h-[1.8rem] w-[40rem] text-[1.5rem] font-medium"
+          />
+        )}
 
         {/* 기타 아이콘 */}
-        <div className="flex flex-wrap justify-between">
-          <FaCommentAlt size={25} />
-          <div className="pr-[32rem]">
-            <FaBookmark size={25} />
+        {isEdit ? (
+          <div className="flex flex-wrap justify-between">
+            <FaCommentAlt size={25} />
+            <div className="pr-[32rem]">
+              <FaBookmark size={25} />
+            </div>
+            <FaRegTrashAlt size={25} />
+            <FaPen size={25} onClick={toggleIsEdit} />
           </div>
-          <FaRegTrashAlt size={25} />
-          <FaPen size={25} />
-        </div>
+        ) : (
+          <div className="flex flex-wrap justify-between">
+            <p className="text-[1.2rem] font-medium text-eventoPurple">
+              구독자에게 공개
+            </p>
+            <FaToggleOn size={25} />
+            <div className="ml-[17rem] h-[3rem] w-[5.2rem] rounded-[0.5rem] border-[0.1rem] border-solid border-eventoPurple text-center text-[1.2rem] font-medium leading-[3rem] text-eventoPurple">
+              취소
+            </div>
+            <div
+              onClick={toggleIsEdit}
+              className="h-[3rem] w-[5.2rem] rounded-[0.5rem] border-[0.1rem] border-solid bg-eventoPurple text-center text-[1.2rem] font-medium leading-[3rem] text-eventoWhite"
+            >
+              저장
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
