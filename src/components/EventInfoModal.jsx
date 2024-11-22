db49a220f5f9c31413ca65109762bb855dd079b7
sujinnn <nmnm0919@naver.com>
2024-11-22 11:28:28 +0900

    Feat: 이벤트 정보 모달 UI 색상 팔레트 적용

--- src/components/EventInfoModal.jsx
+++ src/components/EventInfoModal.jsx
@@ -12,7 +12,7 @@ import {
 export default function EventInfo() {
   return (
     <div className="ml-[18rem] flex h-screen items-center justify-center">
-      <div className="h-[450px] w-[750px] rounded-[20px] p-[40px] shadow-[0px_0px_10px_gray]">
+      <div className="h-[450px] w-[750px] rounded-[20px] bg-eventoWhite p-[40px] shadow-[0px_0px_10px_gray]">
         <div className="flex flex-wrap justify-between">
           {/* 제목 */}
           <div className="text-[64px] font-bold">저녁 약속</div>
@@ -21,18 +21,18 @@ export default function EventInfo() {
           </div>
           <FaXmark size={25} />
         </div>
-        <div className="mb-[50px] w-[160px] rounded-[50px] bg-[#FFC960] text-center text-[20px] font-bold">
+        <div className="bg-eventoYellow mb-[50px] w-[160px] rounded-[50px] text-center text-[20px] font-bold">
           Pooding팀
         </div>
 
         {/* 시간 */}
-        <div className="text-[16px] font-bold text-[#8867DF]">시간</div>
+        <div className="text-[16px] font-bold text-eventoPurple">시간</div>
         <div className="mb-[20px] w-[700px] text-[32px] font-medium">
           2024-9-10 - 2024-9-10
         </div>
 
         {/* 일정 상세 */}
-        <div className="text-[16px] font-bold text-[#8867DF]">일정 상세</div>
+        <div className="text-[16px] font-bold text-eventoPurple">일정 상세</div>
         <div className="w-[700px] pb-[100px] text-[24px] font-medium">
           고기 먹자!
         </div>
