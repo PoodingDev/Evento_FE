f34ae4e756fee01db3e5a1ab3a975e15822e1729
sujinnn <nmnm0919@naver.com>
2024-11-26 16:24:27 +0900

    Feat: #6 이벤트 생성 모달 UI 수정

--- src/components/CreateEventModal.jsx
+++ src/components/CreateEventModal.jsx
@@ -10,28 +10,28 @@ export default function CreateEvent() {
 
   return (
     <div className="ml-[18rem] flex h-screen items-center justify-center pt-[5rem]">
-      <div className="h-[28rem] w-[48rem] rounded-[2.5rem] bg-eventoWhite p-[2.8rem] shadow-[0_0_2.5rem_gray]">
+      <div className="h-[29rem] w-[43rem] rounded-[1.25rem] bg-eventoWhite p-[2.8rem] shadow-xl shadow-lightGray/50">
         <div className="flex flex-wrap justify-between">
           {/* 제목 */}
           <input
             type="text"
             placeholder="일정을 입력하세요"
-            className="mb-[0.5rem] w-[30rem] bg-transparent text-[2.8rem] font-bold"
+            className="mb-[0.8rem] w-[30rem] bg-transparent text-[4rem] font-bold"
           />
           <FaXmark size={25} />
         </div>
 
-        <div className="mb-[2.8rem] flex w-[10rem] justify-center rounded-[3rem] bg-eventoYellow text-center text-[1.2rem] font-bold leading-[1.7rem]">
+        <div className="mb-[2.37rem] flex h-[2rem] w-[10rem] justify-center rounded-[2.5rem] bg-eventoYellow text-center text-[1.25rem] font-bold leading-[2rem]">
           <FaCaretDown size={25} />
           <p>Pooding팀</p>
         </div>
 
         {/* 시간 */}
 
-        <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
+        <div className="mb-[0.25rem] text-[1rem] font-bold text-eventoPurple">
           시간
         </div>
-        <div className="mb-[1.8rem] flex text-[2rem] font-medium">
+        <div className="mb-[2rem] flex text-[2rem] font-bold">
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
@@ -48,7 +48,7 @@ export default function CreateEvent() {
         </div>
 
         {/* 일정 상세 */}
-        <div className="mb-[0.5rem] text-[1rem] font-bold text-eventoPurple">
+        <div className="mb-[0.25rem] text-[1rem] font-bold text-eventoPurple">
           일정 상세
         </div>
 
@@ -59,16 +59,20 @@ export default function CreateEvent() {
         />
 
         {/* 기타 아이콘 */}
-        <div className="flex flex-wrap justify-between">
-          <p className="text-[1.2rem] font-medium leading-[3rem] text-eventoPurple">
-            구독자에게 공개
-          </p>
-          <FaToggleOn size={25} className="mt-[0.7rem]" />
-          <div className="ml-[20rem] h-[3rem] w-[5.2rem] rounded-[0.5rem] border-[0.1rem] border-solid border-eventoPurple text-center text-[1.2rem] font-medium leading-[3rem] text-eventoPurple">
-            취소
+        <div className="flex justify-between">
+          <div className="flex">
+            <p className="w-[8rem] text-[1.2rem] font-medium leading-[3rem] text-eventoPurple">
+              구독자에게 공개
+            </p>
+            <FaToggleOn size={25} className="mt-[0.7rem]" />
           </div>
-          <div className="h-[3rem] w-[5.2rem] rounded-[0.5rem] border-[0.1rem] border-solid bg-eventoPurple text-center text-[1.2rem] font-medium leading-[3rem] text-eventoWhite">
-            저장
+          <div className="flex space-x-[0.5rem]">
+            <button className="flex h-[3rem] w-[5.5rem] items-center justify-center rounded-[0.5rem] border-[0.15rem] border-solid border-eventoPurple text-center text-[1.2rem] text-eventoPurple hover:bg-eventoPurpleLight/50 active:bg-eventoPurpleLight">
+              <span>취소</span>
+            </button>
+            <button className="flex h-[3rem] w-[5.5rem] items-center justify-center rounded-[0.5rem] bg-eventoPurple text-center text-[1.2rem] text-eventoWhite hover:bg-eventoPurple/80 active:bg-eventoPurple/60">
+              <span>저장</span>
+            </button>
           </div>
         </div>
       </div>
