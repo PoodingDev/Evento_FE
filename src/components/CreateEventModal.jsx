572e9566aeb43d1755fac12940b108d02f9a8f55
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-27 19:25:18 +0900

    Feat: #43 이벤트 정보,수정 모달

--- src/components/CreateEventModal.jsx
+++ src/components/CreateEventModal.jsx
@@ -15,7 +15,7 @@ export default function CreateEvent({ onClose }) {
         className="absolute right-[1.2rem] top-[1.2rem] cursor-pointer text-darkGray"
         onClick={onClose}
       />
-      <div className="flex w-full flex-col">
+      <div className="flex flex-col w-full">
         <div className="mb-[1rem] flex items-center justify-between">
           {/* 제목 */}
           <input
