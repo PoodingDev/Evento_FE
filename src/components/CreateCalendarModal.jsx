3612b61a638e12f6ad83e60c04cbcbbba2c24e51
sujinnn <nmnm0919@naver.com>
2024-11-27 16:48:41 +0900

    Feat: #17 캘린더 생성 모달 색상 선택, 공개 토글 수정

--- src/components/CreateCalendarModal.jsx
+++ src/components/CreateCalendarModal.jsx
@@ -1,9 +1,16 @@
-import React, { useRef } from "react";
-import { FaToggleOn } from "react-icons/fa";
+import React, { useRef, useState } from "react";
+import { FaToggleOn, FaToggleOff } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 
 export default function CreateCalendar({ onClose }) {
   const inputRefs = useRef([]);
+
+  //공개 여부
+  const [isPublic, setIsPublic] = useState(false);
+  const toggleIsPublic = () => setIsPublic(!isPublic);
+
+  //색상
+  const [calColor, setCalColor] = useState("calendarRed");
 
   return (
     <div className="w-[43rem flex h-[29rem] w-[37rem] translate-x-[3rem] justify-center rounded-[1.25rem] bg-eventoWhite p-[2.8rem] shadow-xl shadow-lightGray/50">
@@ -40,10 +47,19 @@ export default function CreateCalendar({ onClose }) {
           </div>
           <div className="flex items-center space-x-[0.5rem] text-[1rem] font-bold text-darkGray/80">
             <p>비공개 캘린더로 설정하기</p>
-            <FaToggleOn
-              size={25}
-              className="cursor-pointer text-eventoPurple"
-            />
+            {isPublic ? (
+              <FaToggleOff
+                size={25}
+                className="cursor-pointer text-eventoPurple"
+                onClick={toggleIsPublic}
+              />
+            ) : (
+              <FaToggleOn
+                size={25}
+                className="cursor-pointer text-eventoPurple"
+                onClick={toggleIsPublic}
+              />
+            )}
           </div>
         </div>
 
@@ -53,13 +69,62 @@ export default function CreateCalendar({ onClose }) {
             색상
           </div>
           <div className="flex h-[3rem] w-[11rem] flex-wrap items-center rounded-[0.2rem] p-[0.5rem]">
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarYellow"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarRed"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarGreen"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarLightBlue"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarDarkPurple"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarBlue"></div>
-            <div className="mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarPurple"></div>
+            <button
+              onClick={() => setCalColor("calendarYellow")}
+              className={
+                calColor === "calendarYellow"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarYellow"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarYellow"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarRed")}
+              className={
+                calColor === "calendarRed"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarRed"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarRed"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarGreen")}
+              className={
+                calColor === "calendarGreen"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarGreen"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarGreen"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarLightBlue")}
+              className={
+                calColor === "calendarLightBlue"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarLightBlue"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarLightBlue"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarDarkPurple")}
+              className={
+                calColor === "calendarDarkPurple"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarDarkPurple"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarDarkPurple"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarBlue")}
+              className={
+                calColor === "calendarBlue"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarBlue"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarBlue"
+              }
+            ></button>
+            <button
+              onClick={() => setCalColor("calendarPurple")}
+              className={
+                calColor === "calendarPurple"
+                  ? "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] border-[0.1rem] border-darkGray bg-calendarPurple"
+                  : "mb-[0.25rem] mr-[0.3rem] h-[1rem] w-[1rem] bg-calendarPurple"
+              }
+            ></button>
           </div>
         </div>
 
