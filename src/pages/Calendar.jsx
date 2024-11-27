86eadf754f35d24acf831b308e6b47ba26b96408
Chaeyoung Shin <scy0723123@gmail.com>
2024-11-27 12:08:50 +0900

    Feat: 캘린더 페이지 기본 틀

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Calendar() {
   return (
     <>
-      <div className="flex h-[100vh] items-center justify-center bg-eventoPurpleLight/50 pl-[18rem] pt-[5rem] text-center text-2xl">
-        <span>Calendar 개발 중</span>
+      <div className="flex h-[100vh] bg-eventoPurpleLight/50 text-center text-2xl">
+        <div className="ml-[21rem] mt-[6rem] flex h-[calc(100vh-9rem)] w-[calc(100vw-24rem)] items-center justify-center rounded-sm bg-eventoWhite">
+          <span className="">Calendar 개발 중</span>
+        </div>
       </div>
     </>
   );
