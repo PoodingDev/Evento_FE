591565640cf6ddd5dc3251bf649b50061c2527ce
sujinnn <nmnm0919@naver.com>
2024-11-27 14:31:49 +0900

    Feat: #59 프로필 드롭다운 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
+  const [isView, setIsView] = useState(false);
+  const setView = () => {
+    setIsView(!isView);
+  };
+
   return (
     <div className="evento-header absolute flex h-[5rem] w-full cursor-pointer items-center justify-between py-[1.25rem] pl-[2rem] pr-[2rem]">
       <img
@@ -24,8 +29,32 @@ export default function Header() {
       <FontAwesomeIcon
         icon={faUser}
         className="text-2xl text-[#4F378B]"
-        onClick={() => navigate("/profile")}
+        onClick={setView}
       />
+      {/* {isView && <Dropdown isView={isView} />} */}
+      {isView && (
+        <div className="absolute right-[0rem] top-[5rem] flex h-[21.2rem] w-[15.5rem] flex-col items-center justify-center rounded-[1.25rem] bg-eventoWhite">
+          <FontAwesomeIcon
+            icon={faUser}
+            className="mb-[1.3rem] mt-[0.5rem] text-[3rem] text-[#4F378B]"
+          />
+          <p className="mb-[3rem] text-[1.25rem] font-medium">
+            안녕하세요, dlwlrma님.
+          </p>
+          <button
+            className="mb-[1.25rem] h-[2.5rem] w-[10rem] rounded-[3rem] border-[0.1rem] border-solid border-black text-[1.25rem] font-bold"
+            onClick={() => {
+              setView();
+              navigate("/profile");
+            }}
+          >
+            내 프로필 보기
+          </button>
+          <button className="h-[2.5rem] w-[10rem] rounded-[3rem] border-[0.1rem] border-solid border-darkRed text-[1.25rem] font-bold text-darkRed">
+            로그아웃
+          </button>
+        </div>
+      )}
     </div>
   );
 }
