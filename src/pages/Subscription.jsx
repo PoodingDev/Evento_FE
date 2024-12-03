21c9badd5c3c7bb407f22f2bbd5d641a6fff3c0f
Chaeyoung Shin <scy0723123@gmail.com>
2024-12-03 22:19:39 +0900

    Feat: #113 FullCalendar 커스텀

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -215,9 +215,9 @@ function SubscriptionCalender({ openCalendars, toggleSubscription }) {
   return (
     <>
       <div className="relative after:absolute after:bottom-0 after:left-0 after:top-0 after:w-[2px] after:bg-gray-200 after:content-['']"></div>
-      <section className="flex w-1/3 flex-col items-center">
+      <section className="flex flex-col items-center w-1/3">
         <h1>구독한 캘린더</h1>
-        <ul className="flex w-auto flex-col">
+        <ul className="flex flex-col w-auto">
           {openCalendars.map((calendar) => (
             <li
               key={calendar.calendar_id}
