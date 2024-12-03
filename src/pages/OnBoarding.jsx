d707f470e82936def4a395c91f488f2c63bc1d4d
kimhoseon <kmad910119@gmail.com>
2024-12-04 02:05:46 +0900

    Feat : #64 온보딩 페이지 내용 및 CSS 수정

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -10,29 +10,58 @@ export default function OnBoarding() {
       body: "evento.와 함께 시작하세요",
     },
     {
-      img: "/assets/onboarding/editCalendar.png",
+      img: "/assets/onboarding/main_calendar.png",
       title: "Evento1",
-      body: "이벤토이 이벤토이 벤토설이벤토 설명설 명설 명이벤 ",
+      body: (<>
+        선택한 캘린더만 표시되고, 지정된 색상으로 구분되어
+        <br />
+        한눈에 쉽게 확인할 수 있는 맞춤형 캘린더 서비스입니다! 💖
+      </>)
     },
     {
-      img: "/assets/onboarding/editCalendar.png",
+      img: "/assets/onboarding/subscription_search.png",
       title: "Evento2",
-      body: "이벤토이 이벤토이 벤토설이벤토 설명설 명설 명이벤 ",
+      body: (<>
+        좋아하는 아티스트를 검색해 캘린더를 구독하고,
+        <br />
+        구독한 캘린더를 한눈에 확인할 수 있어요!
+      </>)
     },
     {
-      img: "/assets/onboarding/editCalendar.png",
+      img: "/assets/onboarding/subscription_list.png",
       title: "Evento3",
-      body: "이벤토이 이벤토이 벤토설이벤토 설명설 명설 명이벤 ",
+      body: (<>
+        원하는 캘린더를 선택해,
+        <br />
+        필요한 일정만 한눈에 확인할 수 있는 페이지입니다!
+      </>)
     },
     {
-      img: "/assets/onboarding/editCalendar.png",
+      img: "/assets/onboarding/comment_like.png",
       title: "Evento4",
-      body: "이벤토이 이벤토이 벤토설이벤토 설명설 명설 명이벤 ",
+      body: (<>
+        상세 일정을 클릭해 의견을 나누고,
+        <br />
+        '좋아요'를 눌러보세요!
+      </>)
     },
     {
-      img: "/assets/onboarding/editCalendar.png",
+      img: "/assets/onboarding/d_day.png",
       title: "Evento5",
-      body: "이벤토이 이벤토이 벤토설이벤토 설명설 명설 명이벤 ",
+      body: (<>
+        중요한 일정은 D-DAY로 표시되어,
+        <br />
+        이벤트까지 남은 일수를 한눈에 확인할 수 있습니다.
+      </>)
+    },
+    {
+      img: "/assets/onboarding/create_calendar.png",
+      title: "Evento6",
+      body: (<>
+        색상을 변경하고, 공개/비공개 설정으로 검색 여부를 조정할 수 있어요.
+        <br />
+        친구를 초대해 함께 캘린더를 관리할 수 있습니다!
+      </>)
     },
     {
       img: "/assets/evento_logo.png",
@@ -72,14 +101,12 @@ export default function OnBoarding() {
               className="flex w-full flex-shrink-0 flex-col items-center text-center"
               style={{ width: "100%" }}
             >
-              <div
-                className="mb-5 h-64 w-64 bg-cover bg-center"
-                style={{
-                  backgroundImage: `url(${slide.img})`,
-                  backgroundSize: "contain",
-                  backgroundRepeat: "no-repeat",
-                }}
-              ></div>
+              {/* 이미지 렌더링 */}
+              <img
+                src={slide.img}
+                alt={slide.title}
+                className="mb-5 h-[300px] w-auto object-contain"
+              />
               <h2 className="mb-3 text-2xl font-bold text-eventoPurpleDark">
                 {slide.title}
               </h2>
