23d71672032cb35c8b7388e3c15d834b2dca4539
Chaeyoung Shin <scy0723123@gmail.com>
2024-12-04 18:19:00 +0900

    instance 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// src/api/axios.js
-
 export const instance = axios.create({
-  baseURL: backendBaseURL,
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
