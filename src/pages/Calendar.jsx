00c9b3803ccc0d772936729239e7e22c4193f266
sujinnn <nmnm0919@naver.com>
2024-12-05 16:44:37 +0900

    Feat: #136 목업데이터 이벤트 캘린더에 띄우기

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -6,13 +6,14 @@ import React, { useEffect, useState } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { FaPlus } from "react-icons/fa6";
+import axios from "axios";
 
 export default function Calendar() {
   const [isCreateEventOpen, setIsCreateEventOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
   const [events, setEvents] = useState([]); // 이벤트 상태s
   const [selectedEvent, setSelectedEvent] = useState(null); // 클릭된 이벤트 상태 관리
-
+  const [eventData, setEventData] = useState([]);
   const toggleCreateEvent = () => {
     setIsCreateEventOpen((prev) => !prev);
   };
@@ -20,14 +21,14 @@ export default function Calendar() {
   // 클릭된 이벤트를 처리하는 함수
   const handleEventClick = (info) => {
     const clickedEvent = info.event;
-
+    console.log(clickedEvent);
     const eventDetails = {
       id: clickedEvent.id,
       title: clickedEvent.title,
       start: clickedEvent.start,
       end: clickedEvent.end,
       description: clickedEvent.extendedProps.memo,
-      groupId: clickedEvent.groupId,
+      // groupId: clickedEvent.groupId,
       cal_title: clickedEvent.extendedProps.calTitle,
       color: clickedEvent.backgroundColor,
     };
@@ -40,6 +41,44 @@ export default function Calendar() {
     setIsModalOpen(false); // 모달 닫기
     setSelectedEvent(null); // 선택된 날짜 초기화
   };
+
+  //이벤트 정보 가져오기
+  useEffect(() => {
+    async function fetchEventInfo() {
+      try {
+        const token = localStorage.getItem("token"); // 로컬 스토리지에서 토큰 가져오기
+        const response = await axios.get("/api/calendars/:calendar_id/events", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const start_date = new Date(response.data.start_time);
+        const end_date = new Date(response.data.end_time);
+
+        const formattedStartDate = start_date.toLocaleDateString("ko");
+        const formattedEndDate = end_date.toLocaleDateString("ko");
+
+        const eventMockData = response.data.map((event) => ({
+          allDay: true,
+          id: event.event_id,
+          title: event.event_title,
+          start: event.start_time,
+          end: event.end_time,
+          extendedProps: {
+            memo: event.event_description,
+          },
+          calTitle: event.cal_title,
+          color: event.cal_color,
+          editable: true, // 이벤트 편집 가능
+        }));
+
+        setEvents(eventMockData);
+      } catch (error) {
+        console.error("이벤트 정보를 가져오는 중 오류 발생:", error);
+      }
+    }
+    fetchEventInfo();
+  }, []);
 
   return (
     <>
@@ -84,6 +123,7 @@ export default function Calendar() {
           <CreateEvent onClose={toggleCreateEvent} setEvents={setEvents} />
         </div>
       )}
+      {console.log("dk", events)}
     </>
   );
 }
