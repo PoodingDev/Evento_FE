e6374b6444b2ed5bb24126a4fb9a7b7e43ff38de
Chaeyoung Shin <scy0723123@gmail.com>
2024-12-06 21:09:54 +0900

    소셜 로그인

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
-// // 배포 용
-// export const instance = axios.create({
-//   baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
-//   timeout: 5000,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// });
-
-export const instance = axios.create({
-  baseURL: "/",
-});
-
+export let instance;
+if (import.meta.env.VITE_NODE_ENV === "development") {
+  instance = axios.create({
+    baseURL: "/",
+  });
+} else {
+  instance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+    timeout: 5000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
+}
 export default instance;
