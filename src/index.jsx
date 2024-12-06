eef9a8cffc80fb4dedffb34807e86fd794ca68b9
Chaeyoung Shin <scy0723123@gmail.com>
2024-12-06 14:13:41 +0900

    MSW 파일 제거

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,6 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { worker } from "./mocks/browser";
-
-// // 개발 환경에서만 MSW 시작
-// if (import.meta.env.VITE_NODE_ENV === "development") {
-//   worker.start({
-//     onUnhandledRequest: "bypass",
-//   });
-// }
 
 const container = document.getElementById("root");
 const root = createRoot(container);
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    // headers: {
-    //   "Cross-Origin-Opener-Policy": "same-origin",
-    //   "Cross-Origin-Embedder-Policy": "require-corp",
-    // },
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
   },
   plugins: [react()],
   resolve: {
